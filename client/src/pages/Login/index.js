6704334e292c6f870fdb1229a334b50580a2a681
Shraddha Dhungana <shraddhadhungana12@gmail.com>
2023-04-16 01:32:05 -0400

    Revert "Login / Signup page"
    This reverts commit 1b759d1968487e599071e398cc0e6361850cd4d9.

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,99 +1,53 @@
 import * as React from 'react';
-import { Button, TextField, Typography, Box} from '@mui/material'
+import { Button, Grid, Grow, TextField, Slide} from '@mui/material'
 import { Link } from 'react-router-dom';
 
 
-// export default function Login() {
-  // const minRowSpacing = 0;
-  // const expandedRowSpacing = 3;
-  // const [rowSpacing, setSpacing] = React.useState(minRowSpacing);
-  // const [login, showLogin] = React.useState(false);
-  // const loginContainerRef = React.useRef(null);
+export default function Login() {
+  const minRowSpacing = 0;
+  const expandedRowSpacing = 3;
+  const [rowSpacing, setSpacing] = React.useState(minRowSpacing);
+  const [login, showLogin] = React.useState(false);
+  const loginContainerRef = React.useRef(null);
 
-  const Login = () => {
-    const [isSignUp, setIsSignUp] = React.useState(false);
-    const [input, setInput] = React.useState({
-      username: "",
-      email: "",
-      password: ""
-    });
-
-    const handleChange = (e) => {
-      setInput((prevState) => ({
-        ...prevState,
-         [e.target.name]: e.target.value
-      }))
-    }
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(input);
-    }
-
-    const resetState =() =>{
-      setIsSignUp(!isSignUp);
-      setInput({username:"", email: "", password: ""});
-    }
-    // console.log(isSignUp);
-  //   showLogin((prev) => !prev)
-  //   setSpacing((current) => current === minRowSpacing ? expandedRowSpacing : minRowSpacing);
-  // }
+  const displayLogin = () => {
+    showLogin((prev) => !prev)
+    setSpacing((current) => current === minRowSpacing ? expandedRowSpacing : minRowSpacing);
+  }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <Box display = "flex" 
-        flexDirection = {"column"}
-         maxWidth= {420}
-          alignItems="center" 
-          justifyContent={"center"}
-          margin= "auto"
-          marginTop = {5}
-          padding = {5}
-          borderRadius= {5}
-          boxShadow = {"5px 5px 10px #64ffda"}
-          sx= {{ ":hover" : {boxShadow: "10px 10px 20px #64ffda",}
-          }}
-
-          >
-           
-
-          <Typography variant= "h2" padding= {3} textAlign="center"> {isSignUp ? "SignUp" : "Login"}</Typography>
-          {isSignUp && <TextField onChange = {handleChange} 
-          name = "username" 
-          value = {input.username}
-           margin = "normal" 
-           type ={"text"} 
-           variant="outlined"
-          placeholder='Username' />}
-
-          <TextField onChange = {handleChange}
-           name = "email" 
-           value = {input.email}
-          margin = "normal"
-            type ={"email"}
-            variant="outlined"
-            placeholder='Email' />
-
-          <TextField 
-          onChange = {handleChange}
-           name = "password"
-            value = {input.password}
-             margin = "normal"
-              type ={"password"}
-               variant="outlined"
-                placeholder='Password' />
-
-          <Button type ="submit" sx = {{marginTop:3, borderRadius:3}} variant= "contained" color= "primary" > {isSignUp ? "SignUp" : "Login"}</Button>
-
-          <Button onClick = {resetState}   sx = {{marginTop:3, borderRadius:3}}>
-            {isSignUp ? "Login" : "SignUp"}
-          </Button>
-        </Box>
-      </form>
-    </div>
-    
+    <Grid container columns={12} rowSpacing={rowSpacing} columnSpacing={3} alignContent={'center'} minHeight="100vh">
+      <Grid item xs={12} align="center">
+        {!login ? <Button variant="contained" onClick={displayLogin}>member sign in</Button> : null}
+      </Grid>
+      <Grid item xs={6} align="right" >
+        <Grow in={login}>
+          <TextField required label="Username"/>
+        </Grow>
+        
+      </Grid>
+      <Grid item xs={6} align="left">
+        <Grow in={login} style={{transformOrigin: '0 0 0'}} {...(login ? { timeout: 1000 } : {})}>
+          <TextField required label="Password"/>
+        </Grow>
+      </Grid>
+      <Grid item xs={5} />
+      <Grid item xs={2} justifyContent={'center'} align="center" ref={loginContainerRef}>
+        {!login ? 
+        <Button variant="contained" id='proceedButton'>continue as guest</Button> : 
+        <Slide direction='right' in={login} container={loginContainerRef.current}>
+          <Button variant="contained" id='proceedButton'>Login</Button>
+        </Slide> 
+        }
+      </Grid>
+      <Grid item xs={5} />
+      <Grid item xs={12} align="center">
+        {!login? null : 
+        <Slide direction="right" in={login} container={loginContainerRef.current}>
+          <Button variant="text" size='small'>continue as guest</Button>
+        </Slide>
+        }
+      </Grid>
+    </Grid>
   );
-};
-
-export default Login
+}
