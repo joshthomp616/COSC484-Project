e4bf175c41c3f545766f99c3c4c43c9ca33ed576
Shraddha Dhungana <shraddhadhungana12@gmail.com>
2023-05-14 11:37:57 -0400

    Home page fixed to be fit in every screen size.
    Home page css fixed and can be displayed in every screen size

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../nav'
 import './home.css';
-import { Container,Button } from '@mui/material';
+import { Container,Button,TextField, Typography, Grid, Paper } from '@mui/material';
 
 export default function Home() {
   return (
@@ -10,16 +10,21 @@ export default function Home() {
     <Navbar />
     <div className= 'home-container'>
       <video src = "/video1.mp4" autoPlay loop muted />
+
+      <div style={{ textAlign: 'center'}}> 
       <h1> WELCOME TO AUTO SPA</h1>
-      <h2> A New Era In The Automotive Service</h2>
-      <p> Schedule Your Next Appointment With Us</p>
+      <p> A New Era In The Automotive Service</p>
+      </div>
+      </div>
+      
 
       <div className = 'home-btns'>
+      <h2> Schedule Your Next Appointment With Us</h2>
         { <Button href ="/appointment" 
           sx = {{marginTop:3, borderRadius:3}} variant= "contained" color= "secondary" size= "large" >
           Request Appointment</Button>  }
         </div>
-    </div>
+    
     </>
   );
 }
