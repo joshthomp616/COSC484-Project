8426059ef8fca6582e3f0f52065a33dd66d66082
dhamid1 <125397550+dhamid1@users.noreply.github.com>
2023-05-16 02:57:37 -0400

    Added adminAppoinmentPage

--- client/src/pages/AdminAppoinment/index.js
+++ client/src/pages/AdminAppoinment/index.js
@@ -1,48 +1,121 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import './admin.css'; // Import the CSS file for styling
 
 const AppointmentForm = () => {
   const [appointments, setAppointments] = useState([]);
+  const [customerId, setCustomerId] = useState('');
+  const [serviceId, setServiceId] = useState('');
+  const [date, setDate] = useState('');
 
-  useEffect(() => {
-    fetchAppointments();
-  }, []);
-
-  const fetchAppointments = async () => {
+  const fetchAppointmentsByCustomerId = async () => {
     try {
-      const response = await axios.get('https://autospaapi.azurewebsites.net/Appointment');
+      const response = await axios.get(`https://autospaapi.azurewebsites.net/Appointment/customer/${customerId}`);
       setAppointments(response.data);
     } catch (error) {
       console.error(error);
     }
   };
 
+  const fetchAppointmentsByServiceId = async () => {
+    try {
+      const response = await axios.get(`https://autospaapi.azurewebsites.net/Appointment/service/${serviceId}`);
+      setAppointments(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchAppointmentsByDate = async () => {
+    try {
+      const response = await axios.get(`https://autospaapi.azurewebsites.net/Appointment/appointmentsDate/${date}`);
+      setAppointments(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchAppointmentByCustomerIdAndDate = async () => {
+    try {
+      const response = await axios.get(`https://autospaapi.azurewebsites.net/Appointment/customerAppointment/${customerId}/${date}`);
+      setAppointments(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCustomerIdChange = (event) => {
+    setCustomerId(event.target.value);
+  };
+
+  const handleServiceIdChange = (event) => {
+    setServiceId(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
   return (
-    <div>
-      <h2>Appointments:</h2>
-      <button onClick={fetchAppointments}>Load Appointments</button>
-      <table>
-        <thead>
-          <tr>
-            <th>Customer ID</th>
-            <th>Date</th>
-            <th>Service ID</th>
-            <th>Vehicle ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          {appointments.map((appointment, index) => (
-            <tr key={index}>
-              <td>{appointment.customerId}</td>
-              <td>{appointment.dateOfAappointment}</td>
-              <td>{appointment.serviceId}</td>
-              <td>{appointment.vehicleId}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    <div className="appointment-page">
+      <h2>Appointments</h2>
+
+      <div className="input-group">
+        <label htmlFor="customerId">Customer ID:</label>
+        <input type="text" id="customerId" value={customerId} onChange={handleCustomerIdChange} />
+
+        <button className="spaced-button" onClick={fetchAppointmentsByCustomerId}>Get by Customer ID</button>
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="serviceId">Service ID:</label>
+        <input type="text" id="serviceId" value={serviceId} onChange={handleServiceIdChange} />
+
+        <button className="spaced-button" onClick={fetchAppointmentsByServiceId}>Get by Service ID</button>
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="date">Date:</label>
+        <input type="text" id="date" value={date} onChange={handleDateChange} />
+
+        <button className="spaced-button" onClick={fetchAppointmentsByDate}>Get by Date</button>
+      </div>
+
+      <div className="input-group">
+        <label htmlFor="customerIdAndDate">Customer ID:</label>
+        <input type="text" id="customerIdAndDate" value={customerId} onChange={handleCustomerIdChange} />
+
+        <label htmlFor="date">Date:</label>
+        <input type="text" id="date" value={date} onChange={handleDateChange} />
+
+        <button className="spaced-button" onClick={fetchAppointmentByCustomerIdAndDate}>Get by Customer ID and Date</button>
+      </div>
+
+      {appointments.length > 0 && (
+  <table className="appointment-table">
+    <thead>
+      <tr>
+        <th>Customer ID</th>
+        <th>Date</th>
+        <th>Service ID</th>
+        <th>Vehicle ID</th>
+      </tr>
+    </thead>
+    <tbody>
+      {appointments.map((appointment, index) => (
+        <tr key={index}>
+          <td>{appointment.customerId}</td>
+          <td>{appointment.dateOfAappointment}</td>
+          <td>{appointment.serviceId}</td>
+          <td>{appointment.vehicleId}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+)}
+</div>
+);
 };
 
 export default AppointmentForm;
+
--- server/index.js
+++ server/index.js
@@ -2,18 +2,14 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
-const PORT = process.env.PORT || 3001;
-
+app.use(cors()); 
 const app = express();
 app.use(cors()); 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-  }); 
-
+const PORT = process.env.PORT || 3001;
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!"});
 });
